37de9fc37bc14e73a15205c4e30c55be3a3152bc
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-10-22 01:50:49 -0300

    feat: :tada: first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        sans: "var(--font-roboto)",
       },
     },
   },
