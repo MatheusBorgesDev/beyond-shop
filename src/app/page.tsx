90528c28b7321c8d4762a2bde8f40874c23ea204
MatheusBorgesDev <matheusb.dev@gmail.com>
2024-10-28 22:40:25 -0300

    feat: :sparkles: add product cards to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import product1 from "../assets/t-shirt-1.png";
+import product2 from "../assets/t-shirt-2.png";
+import product3 from "../assets/t-shirt-3.png";
+import { ProductCard } from "./components/product-card";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="flex h-1 w-5 gap-1">Home</h1>
-    </div>
+    <main className="ml-auto flex min-h-[656px] w-full max-w-[calc(100vw-((100vw-1180px)/2))] gap-3 py-4">
+      <ProductCard src={product1} alt="" />
+      <ProductCard src={product2} alt="" />
+      <ProductCard src={product3} alt="" />
+    </main>
   );
 }
